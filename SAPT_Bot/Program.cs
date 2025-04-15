557ac13055ada5c189dcdfc42fa9a58cc0c8f9d5
kertAW <kinogeras@gmail.com>
2025-04-15 08:24:39 +0300

    first commit

---
+++ SAPT_Bot/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using SAPT_Bot.Routes;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace SAPT_Bot
+{
+    public class Program
+    {
+        private static ScheduleRoute scheduleRoute;
+
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Рассмотрите возможность добавления модификатора "required" или объявления значения, допускающего значение NULL.
+        public static TelegramBotClient Bot { get; set; }
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Рассмотрите возможность добавления модификатора "required" или объявления значения, допускающего значение NULL.
+
+        static void Main(string[] args)
+        {
+            using var cts = new CancellationTokenSource();
+            var bot = new TelegramBotClient("7970806482:AAHG-K-uUONSXCGmw7lvdFsOyqwFuu3lkXs", cancellationToken: cts.Token);
+
+            Bot = bot;
+
+            var me = bot.GetMe();
+            me.Wait();
+
+            bot.OnMessage += OnMessage;
+            bot.OnUpdate += Bot_OnUpdate;
+
+            scheduleRoute = new ScheduleRoute(bot);
+
+            Console.WriteLine($"@{me.Result.Username} is running... Press Enter to terminate.");
+            Console.ReadLine();
+            cts.Cancel();
+        }
+
+        private async static Task Bot_OnUpdate(Update update)
+        {
+            // Если inline keyboard button - то update.CallbackQuery
+            if (update.CallbackQuery != null)
+            {
+                Console.WriteLine($"Received id=\"{update.CallbackQuery.Id}\" with data \"{update.CallbackQuery.Data}\"");
+                await Bot.AnswerCallbackQuery(update.CallbackQuery.Id);
+
+                switch (update.CallbackQuery.Data)
+                {
+                    case "menuButton_scheduleMailing":
+                        await scheduleRoute.GoToScheduleMailing(update.CallbackQuery.Message);
+                        break;
+                    case "menuButton_guide":
+                        break;
+                    case "menuButton_reportProblem":
+                        break;
+                    case "menuButton_authors":
+                        break;
+                    case "schedule_fullSchedule":
+                        await scheduleRoute.GetFullScheduleChoice(update.CallbackQuery.Message);
+                        break;
+                    case "schedule_todayFullSchedule":
+                        await scheduleRoute.GetFullSchedule(update.CallbackQuery.Message, FullScheduleMode.Today);
+                        break;
+                    case "schedule_tomorrowFullSchedule":
+                        await scheduleRoute.GetFullSchedule(update.CallbackQuery.Message, FullScheduleMode.Tomorrow);
+                        break;
+                    case "schedule_changeGroup":
+                        break;
+                }
+            }
+        }
+
+        private async static Task OnMessage(Message message, UpdateType type)
+        {
+            if (message.Text is null) return;
+
+            Console.WriteLine($"Received {type} '{message.Text}' in {message.Text}");
+            await Bot.SendMessage(message.Chat, $"{message.From} said: {message.Text}");
+            await SendMenu(message, type);
+        }
+
+        private async static Task SendMenu(Message message, UpdateType type)
+        {
+            var menuMessage = await Bot.SendMessage(message.Chat.Id, "Это меню!", 
+                ParseMode.Html, 
+                protectContent: true,
+                replyMarkup: new InlineKeyboardButton[][]
+                {
+                    [("Рассылка расписания", "menuButton_scheduleMailing")],
+                    [("Справочник", "menuButton_guide")], 
+                    [("Сообщить о неполадках", "menuButton_reportProblem"), ("Авторы", "menuButton_authors")],
+                });
+        }
+    }
+}
---
+++ SAPT_Bot/Routes/ScheduleRoute.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace SAPT_Bot.Routes
+{
+    public class ScheduleRoute
+    {
+        private TelegramBotClient bot;
+
+        public ScheduleRoute(TelegramBotClient bot)
+        {
+            this.bot = bot;
+        }
+
+        public async Task GoToScheduleMailing(Message message)
+        {
+            await bot.SendMessage(message.Chat.Id, "Страница расписаний",
+                replyMarkup: new InlineKeyboardButton[][]
+                {
+                    [("Получить полное расписание", "schedule_fullSchedule")],
+                    [("Выбрать группу", "schedule_changeGroup")]
+                });
+        }
+
+        public async Task GetFullScheduleChoice(Message message)
+        {
+            await bot.SendMessage(message.Chat.Id, "Выберите день, по которому хотите получить расписание",
+                replyMarkup: new InlineKeyboardButton[][]
+                {
+                    [("Сегодня", "schedule_todayFullSchedule"), ("Завтра", "schedule_tomorrowFullSchedule")]
+                });
+        }
+
+        public async Task GetFullSchedule(Message message, FullScheduleMode Mode)
+        {
+            using (var http = new HttpClient())
+            {
+                string date = "";
+
+                if (Mode == FullScheduleMode.Today)
+                {
+                    date = DateTime.Now.ToString("dd.MM");
+                }
+                else
+                {
+                    date = DateTime.Now.AddDays(1).ToString("dd.MM");
+                }
+
+                try
+                {
+                    await bot.SendDocument(message.Chat.Id, $@"https://gbpousapt.ru/public/РАСПИСАНИЕ/{date}.pdf", "Мегахуйня");
+                }
+                catch
+                {
+                    await bot.SendMessage(message.Chat.Id, "На данный день ещё не выложено расписание, либо произошла ошибка");
+                }
+            }
+        }
+    }
+
+    public enum FullScheduleMode
+    {
+        Today,
+        Tomorrow
+    }
+}
